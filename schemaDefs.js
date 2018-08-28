f5a934add8b4faf83a901650c539553e295e289d
ibesoft11 <ibesoft11@gmail.com>
2018-08-28 16:48:17 +0100

    Added Graphql definitions for MySql Views

--- schemaDefs.js
+++ schemaDefs.js
@@ -13,8 +13,132 @@ const typeDefs = `
     pay_period: [pay_period],
     reward_vote: [reward_vote],
     trust_cert: [trust_cert],
+    invoice_summary: [invoice_summary],
+    issue_budget: [issue_budget],
+    issue_budget_unwt: [issue_budget_unwt],
+    issue_budget_wt: [issue_budget_wt],
+    reward: [reward],
+    reward_unwt: [reward_unwt],
+    reward_wt: [reward_wt],
+    slash_judgement: [slash_judgement],
+    task_approval_overdue: [task_approval_overdue],
+    user_flair: [user_flair]
+  }
+
+  type user_flair {
+    login: String,
+	  verified_coop: String,
+	  rating: Int,
+	  rating_label: String,
+	  weight: Float,
+	  sig: String
+  }
+
+  type task_approval_overdue {
+    issue_num: Int,
+	  title: String,
+	  state: String,
+	  days_old: Int,
+	  weeks_old: Int,
+	  labels: String,
+	  createdAt: DateTime,
+	  updatedAt: DateTime
+  }
+
+  type slash_judgement {
+    worker: String,
+	  voters: String,
+	  weight: Float,
+	  issue_num: Int,
+	  pay_period: DateTime
+  }
+
+  type reward_wt {
+    issue_num: Int,
+    title: String,
+    worker: String, 
+    reward_usd: Float,
+    percent_avg: Float,
+    budget_usd: Float,
+    voter_qty: Int,
+    voters: String,
+    reward_provisional: Float,
+    budget_provisional: Float,
+    pay_period: DateTime,
+    labels: String
+  }
+
+  type reward_unwt {
+    issue_num: Int,
+    title: String,
+    worker: String, 
+    reward_usd: Float,
+    percent_avg: Float,
+    budget_usd: Float,
+    voter_qty: Int,
+    voters: String,
+    reward_provisional: Float,
+    budget_provisional: Float,
+    pay_period: DateTime,
+    labels: String
+  }
+
+  type reward {
+    issue_num: Int,
+    title: String,
+    worker: String, 
+    reward_usd: Float,
+    percent_avg: Float,
+    budget_usd: Float,
+    voter_qty: Int,
+    voters: String,
+    reward_provisional: Float,
+    budget_provisional: Float,
+    pay_period: DateTime,
+    labels: String
   }
  
+  type issue_budget_wt {
+    issue_num: Int,
+    title: String,
+    budget_usd: Float,
+    budget_provisional: Float,
+    voter_qty: Int,
+    voters: String,
+    pay_period: DateTime,
+    labels: String
+  }
+
+  type issue_budget_unwt {
+    issue_num: Int,
+    title: String,
+    budget_usd: Float,
+    budget_provisional: Float,
+    voter_qty: Int,
+    voters: String,
+    pay_period: DateTime,
+    labels: String
+  }
+
+  type issue_budget {
+    issue_num: Int,
+    title: String,
+    budget_usd: Float,
+    budget_provisional: Float,
+    voter_qty: Int,
+    voters: String,
+    pay_period: DateTime,
+    labels: String
+  }
+
+  type invoice_summary {
+    pay_period: DateTime!,
+    worker: String,
+    issues: String,
+    USD: Int,
+    RHOC: Float
+  }
+
   type admin_settings {
     id: ID!,
     current_pay_period: DateTime!
